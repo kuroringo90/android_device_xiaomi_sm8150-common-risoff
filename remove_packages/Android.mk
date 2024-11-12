3eb89fcd1efcac274416fbe54f96105716efff48
kuroringo90 <ak4700@gmx.com>
2024-11-12 23:17:00 +0100

    sm8150-common: update remove gapps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Updater LocationHistoryPrebuilt PrebuiltGoogleTelemetryTvp WallpaperEmojiPrebuilt-v470 WallpaperEffect GoogleFeedback
+LOCAL_OVERRIDES_PACKAGES := Updater LocationHistoryPrebuilt PrebuiltGoogleTelemetryTvp WallpaperEmojiPrebuilt-v470 WallpaperEffect GoogleFeedback GooglePrintRecommendationService AmbientStreaming talkback 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
