d2823ccade1ccfa0b40640638d733b447703f797
Pranav Vashi <neobuddy89@gmail.com>
2025-01-08 01:47:16 +0530

    sm8150-common: Remove no longer used display flags
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -185,12 +185,11 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
-    hwcomposer.msmnile
+    hwcomposer.qcom
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.msmnile \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
